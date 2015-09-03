7d9f513a9e8cf9222519116042d9125bb08ffe44
Luis M Diaz <ldiaz679@e112-m-15.mchenry.edu>
2015-09-03 16:55:29 -0500

    Constant And Variables

---
+++ Contents.swift
@@ -0,0 +1,48 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var s1:String = "Lipold Disk Golf" // Name of course
+
+
+var i1:Int = 18 // Number of holes
+
+
+var f1:Float = 58.7 // average score
+
+
+var d1:Double = 189.0 // rounds recorded
+
+
+
+let s3 = "good game"
+
+let i3 = 9 // half the course
+
+let f3 = -10 // my goal
+
+let d3 = 266 // # of yards for hole 1
+
+
+let s2:String = "par"
+
+let i2:Int = 5032 // Course length
+
+let f2: Float = -5 // My average par
+
+let d2:Double = 3.29 // Golf Course rating
+
+let s4 = 100 // Tried to add it to i1 but it wouldnt let me change it
+
+let f4:Float = 4 // It works, does this mean whole numbers can entered as floats?
+
+
+
+
+
+
+
+
